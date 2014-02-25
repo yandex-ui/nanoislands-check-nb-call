89e939dbd2c6e53cdc53cc9adffa5b4aec62c949
Aleksei Androsov <doochik@ya.ru>
2014-02-25 14:22:05 +0400

    move to coa

--- bin/nb-check-call.js
+++ bin/nb-check-call.js
@@ -1,6 +1,56 @@
 #!/usr/bin/env node
 
-var checker = require('../checker');
+require('coa').Cmd()
+    .name(process.argv[1])
+    .title('Dummy checks to prevent XSS in nanoislands yate templates.')
+    .helpful()
+    .opt()
+        .name('version') .title('Version')
+        .short('v').long('version')
+        .flag()
+        .only()
+        .act(function() {
+            return require('../package.json').version;
+        })
+        .end()
+    .opt()
+        .name('input').title('Input path')
+        .short('i').long('input')
+        .req()
+        .end()
+    .opt()
+        .name('ast').title('Write ast for compiled yate-file (default: yes)')
+        .long('freeze')
+        .def(true)
+        .val(function(v) {
+            return stringToBoolean(v, true);
+        })
+        .end()
+    .opt()
+        .name('import').title('Import path for yate modules')
+        .long('import')
+        .end()
+    .act(function(opts) {
+        var checker = require('../checker');
 
-var result = checker.check(process.argv[2]);
-result.errors.explainErrors();
+        var result = checker.check(
+            opts['input'],
+            opts['import'],
+            opts
+        );
+        result.errors.explainErrors();
+    })
+    .run();
+
+function stringToBoolean(s, def) {
+    if (typeof s === 'boolean') {
+        return s;
+    }
+    if (s == 'yes' || s == 'true') {
+        return true;
+    }
+    if (s == 'no' || s == 'false') {
+        return false;
+    }
+    return !!def;
+}
