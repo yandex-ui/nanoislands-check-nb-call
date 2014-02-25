0be671c94a35c82561575d1b1df649505618d63b
Aleksei Androsov <doochik@ya.ru>
2014-02-25 18:12:12 +0400

    ignores nb-input "content" prop

---
+++ test/nb-input-ignores.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var TYPE = require('../lib/errors').TYPE;
+
+describe.only('nb-input-ignores', function() {
+
+    it('should ignore nb-input "content" prop', function() {
+        var name = this.compile('nb-input-ignores-content.yate');
+        var res = this.check(name);
+
+        expect(res.errors.isEmpty()).to.be.equal(true);
+    });
+
+    it('should ignore nb-input "content" prop in multiline', function() {
+        var name = this.compile('nb-input-ignores-content-multiline.yate');
+        var res = this.check(name);
+
+        expect(res.errors.isEmpty()).to.be.equal(true);
+    });
+
+    it('should not ignore nb-input "leftContent" prop', function() {
+        var name = this.compile('nb-input-not-ignores-leftContent.yate');
+        var res = this.check(name);
+
+        expect(res.errors.isEmpty()).to.be.equal(false);
+    });
+
+});
