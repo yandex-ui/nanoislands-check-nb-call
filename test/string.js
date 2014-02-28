598b7dcfdffbee6236b6cf53711a5659fb5f405d
Aleksei Androsov <doochik@ya.ru>
2014-02-28 20:38:29 +0400

    fix but with muliple errors

--- test/string.js
+++ test/string.js
@@ -64,6 +64,39 @@ describe('scalar', function() {
 
     });
 
+    describe('string with <> and interpolation', function() {
+
+        before(function() {
+            this.name = this.compile('string-invalid2.yate');
+            this.result = this.check(this.name).errors;
+        });
+
+        it('should return error', function() {
+            expect(this.result.getErrorCount()).to.be.equal(1);
+        });
+
+        it('should return error with "error" key', function() {
+            expect(this.result.getErrorList()[0]).to.have.property('type', TYPE.STRING_HAS_TAGS);
+        });
+
+        it('should return error with "propName" key', function() {
+            expect(this.result.getErrorList()[0].error).to.have.property('propName', 'content');
+        });
+
+        it('should return error with "propType" key', function() {
+            expect(this.result.getErrorList()[0].error).to.have.property('propType', 'scalar');
+        });
+
+        it('should return error with "propValue" key', function() {
+            expect(this.result.getErrorList()[0].error).to.have.property('propValue', '<span style="background:#');
+        });
+
+        it('should return error with "where" key', function() {
+            expect(this.result.getErrorList()[0]).to.have.property('where').that.is.a('object');
+        });
+
+    });
+
     describe('string with <> as var', function() {
 
         before(function() {
